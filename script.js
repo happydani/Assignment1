6b036c7a3ec4f4761e986d8101f76008a74e996a
Danette Klein <danetteklein@gmail.com>
2016-06-17 22:48:42 -0700

    Adding html and js with steps 1-5 of part 2 pages our assignment.

---
+++ script.js
@@ -0,0 +1,100 @@
+/// STEP 1
+/* var someMonth;
+var function theMonth(CURRENT_MONTH); // function to return current month
+var CURRENT_MONTH; 		              // a constant
+var summerMonth; 		              // an array of summer months
+var myLibraryAFunction(); 	          // a function 
+
+// STEP 2 - various literals
+1        // numeric 
+"This is an example of a string literal"
+true     //boolean 
+null     //null  
+
+
+// STEP 3 - complex/variable expressions
+var anExpression = 3 * (4 / 5) + 6;
+var aSecondExpression = Math.PI * radius * radius;
+
+
+// STEP 4 - variable declarations
+
+var strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, strMayWeContactYou;
+
+
+// STEP 5 - 3 ways to make variable declarations and assignments
+
+var strFirstName;
+var isInstructor;
+strFirstName = "Danette";
+isInstructor = false;
+
+var strFirstName = "Danette";
+var isInstructor = false;
+
+var strFirstName = "Danette", isInstructor = false;
+*/
+alert("Oh snap, a virus has been installed on your computer!");
+
+// STEP 6
+
+//var addNumbersStrings = " strings";
+//window.console.log("addNumbersStrings");
+
+// STEP 7
+
+
+// STEP 8
+
+
+// STEP 9
+
+
+// STEP 10
+
+
+// STEP 11
+
+
+// STEP 12
+
+
+// STEP 13
+
+
+// STEP 14
+
+
+// STEP 15
+
+
+// STEP 16
+
+
+// STEP 17
+
+
+// STEP 18
+
+
+// STEP 19
+
+
+// STEP 20
+
+
+// STEP 21
+
+
+// STEP 22
+
+
+// STEP 23
+
+
+// STEP 24
+
+
+// STEP 25
+
+
